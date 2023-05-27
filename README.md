18502c478975aa60ea4d6a20b851c66a15d51bd1
abrarahmed95 <abrarahmed377@hotmail.com>
2023-05-27 19:56:41 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# next-template
+
+A Next.js 13 template for building apps with fast api
+.
---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000"
+            : "/api",
+      },
+    ]
+  },
+}
+
+export default nextConfig
